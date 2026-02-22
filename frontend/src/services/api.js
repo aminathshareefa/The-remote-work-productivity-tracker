e19675f7d5b53193ccdfd8f70d5308f8a6943f71
aminath shareefa <aminathshareefa.u@gmail.com>
2026-02-22 17:25:01 +0530

    Updated API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://the-remote-work-productivity-tracker.onrender.com' });
+const API = axios.create({ baseURL: 'https://the-remote-work-productivity-tracker.onrender.com/api' });
 
-// Automatically attach Auth Token to every request
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem('token');
     if (token) req.headers.Authorization = `Bearer ${token}`;
