493c10a1ae42544791d071eef6ccabd62e10800b
aminath shareefa <aminathshareefa.u@gmail.com>
2026-02-24 21:12:04 +0530

    Updated API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://the-remote-work-productivity-tracker.onrender.com/api' });
+const API = axios.create({ 
+    baseURL: 'https://the-remote-work-productivity-tracker.onrender.com/api' 
+});
 
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem('token');
-    if (token) req.headers.Authorization = `Bearer ${token}`;
+    if (token) {
+        req.headers.Authorization = `Bearer ${token}`;
+    }
     return req;
 });
 
