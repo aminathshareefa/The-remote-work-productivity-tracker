380e170c265fe29f738fcc93228a55ba639f939f
aminath shareefa <aminathshareefa.u@gmail.com>
2026-02-26 14:58:49 +0530

    Updated api path

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-// Route Imports
 import authRoutes from './routes/authRoutes.js';
 import taskRoutes from './routes/taskRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
@@ -15,40 +14,40 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 
-// CORS Configuration
 app.use(cors({
-    origin: "https://productivity-tracker-five.vercel.app", // നിങ്ങളുടെ Vercel ലിങ്ക്
+    origin: ["https://productivity-tracker-five.vercel.app", "http://localhost:5173"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
 
-// Connect Database
+
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB Connected...");
     } catch (err) {
         console.error("Database Connection Failed:", err.message);
-        process.exit(1);
     }
 };
 connectDB();
 
-// Root Route (To avoid "Cannot GET /" error in browser)
 app.get('/', (req, res) => {
     res.send("Remote Work Productivity Tracker API is running...");
 });
 
-// Routes - match with your api.js baseURL
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/employee', employeeRoutes);
 app.use('/api/manager', managerRoutes);
 app.use('/api/time', timeRoutes);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Internal Server Error' });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
